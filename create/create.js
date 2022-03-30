44ee3867c21c95d2323a02156ffa5eb1b4e9bb8b
Delaney Fogarty <delaney.fk@gmail.com>
2022-03-30 14:28:50 -0700

    create bunny form working

--- create/create.js
+++ create/create.js
@@ -10,10 +10,18 @@ const logoutButton = document.getElementById('logout');
 
 form.addEventListener('submit', async e => {
     // prevent default
-
+  e.preventDefault();
     // get the name and family id from the form
+  const data = new FormData(form);
+
+  const name = data.get('bunny-name');
+  const familyId = data.get('family-id');
 
     // use createBunny to create a bunny with this name and family id
+  await createBunny({
+    name: name,
+    family_id: familyId
+  });
 
   form.reset();
 });
@@ -21,11 +29,17 @@ form.addEventListener('submit', async e => {
 window.addEventListener('load', async () => {
     // let's dynamically fill in the families dropdown from supabase
     // grab the select HTML element from the DOM
-
+  const familySelector = document.querySelector('.family-id');
     // go get the families from supabase
-
+  const families = await getFamilies();
     // for each family
+  for (let family of families) {
+    const optionTag = document.createElement('option');
+    optionTag.textContent = family.name;
+    optionTag.value = family.id;
 
+    familySelector.append(optionTag);
+  }
     // create an option tag
 
     // set the option's value and text content
