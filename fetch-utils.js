69e3d6952b3a395d9e3abaf21a65ef11ba8ab15b
Delaney Fogarty <delaney.fk@gmail.com>
2022-03-30 12:14:32 -0700

    fetch-util fxns written

--- fetch-utils.js
+++ fetch-utils.js
@@ -1,6 +1,8 @@
+/* eslint-disable no-console */
 // Create your own supabase database using the provided seeds.sql file
-const SUPABASE_URL = '';
-const SUPABASE_KEY = '';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJyb2xlIjoiYW5vbiIsImlhdCI6MTYzNjQxMTMxMiwiZXhwIjoxOTUxOTg3MzEyfQ.PHekiwfLxT73qQsLklp0QFEfNx9NlmkssJFDnlvNIcA';
+
+const SUPABASE_URL = 'https://gxwgjhfyrlwiqakdeamc.supabase.co';
 
 const client = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
 
@@ -9,18 +11,30 @@ export function getUser() {
 }
 
 export async function getFamilies() {
+    const response = await client
+    .from('loving_families')
+    .select(`*,
+    fuzzy_bunnies (*)`
+    );
     // fetch all families and their bunnies
 
     return checkError(response);
 }
 
 export async function deleteBunny(id) {
+    let response = await client
+    .from('fuzzy_bunnies')
+    .delete()
+    .match({ id: id });
     // delete a single bunny using the id argument
 
     return checkError(response);
 }
 
 export async function createBunny(bunny) {
+    let response = await client
+    .from('fuzzy_bunnies')
+    .insert(bunny);
     // create a bunny using the bunny argument
 
     return checkError(response);
